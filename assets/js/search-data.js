4ce5761389a0f91385b8528afb85f771d7fc2480
ParkerKai <43325656+ParkerKai@users.noreply.github.com>
2025-10-15 23:30:15 +0000

    Deploying to gh-pages from @ ParkerKai/ParkerKai.github.io@e59adee940a56a638f3d5b71301e3905bd75e636 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume",
+          title: "resume",
+          description: "Resume for download",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "nav-teaching",
           title: "teaching",
